b3ef5aa38c64314ee7037515fecc5f975fd56ae4
dadambi116 <uryzipi@gmail.com>
2025-05-12 17:40:00 +0900

    Feat: add login_page jsx code add public/images, and add login page front-end source code.

--- wlm_front/src/App.jsx
+++ wlm_front/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/component/login.jsx';
 import Notemate from './pages/notemate/component/notemate.jsx';
 
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Notemate />
+//       <Login />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 function App() {
   return (
-    <div className="App">
-      <Notemate />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/notemate" element={<Notemate />} />
+      </Routes>
+    </Router>
   );
 }
 
