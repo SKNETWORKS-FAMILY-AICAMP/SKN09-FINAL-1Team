af02de5817ada1e856f8a05786a809ae5354368a
Leegwangwoon <leegwangwoon@naver.com>
2025-05-26 12:34:13 +0900

    Chor: remove_unused_file

--- wlm_front/src/components/ChatMessage.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './ChatMessage.module.css';
-
-const ChatMessage = ({ user, message }) => (
-  <div className={styles.chatMessage}>
-    <span className={styles.user}>{user}:</span> {message}
-  </div>
-);
-
-export default ChatMessage; 
