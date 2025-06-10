2b5d4bd879affe790fc51d39a8d11feb3ca5cdb8
ohdyo <smart5572@naver.com>
2025-06-10 17:26:15 +0900

    refac: Change install_docker.sh code

--- wlm_front/install_docker.sh
+++ wlm_front/install_docker.sh
@@ -12,17 +12,4 @@ sudo apt update
 
 sudo apt install -y docker-ce
 
-sudo usermod -aG docker $USER
-
-sudo systemctl start docker
-sudo systemctl enable docker
-
-echo "Docker installation completed successfully!"
-
-sudo git clone https://github.com/SKNETWORKS-FAMILY-AICAMP/SKN09-FINAL-1Team.git
-
-sudo cd SKN09-FINAL-1Team/wlm_front
-
-sudo docker build -t wlm_front .
-
-sudo docker run -d -p 5173:5173 --name wlb_front wlb_front
+sudo docker --version
