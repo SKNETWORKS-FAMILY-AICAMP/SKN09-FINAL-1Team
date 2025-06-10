90980354157044f93fd4f4a506d666c1cb506a4b
ohdyo <smart5572@naver.com>
2025-06-10 23:39:58 +0900

    refix: .sh file

---
+++ model_hosting/install_python_ollama.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+apt update
+
+# 시스템 패키지 설치
+apt install -y --no-install-recommends \
+    build-essential \
+    curl \
+    libcudnn8 \
+    libcudnn8-dev \
+    ffmpeg \
+    python3-venv
+
+# Python 가상환경 설정
+python3 -m venv myvenv
+. ./myvenv/bin/activate
+
+# requirements.txt 설치
+pip install --no-cache-dir -r ./SKN09-FINAL-1Team/model_hosting/requirements.txt
+
+# Ollama 설치
+curl -fsSL https://ollama.com/install.sh | sh
+
+# 시작 스크립트 생성
+echo '#!/bin/bash
+nohup ollama serve > ollama.log 2>&1 &
+sleep 10
+ollama pull qwen2.5
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload' > start.sh
+
+# 시작 스크립트에 실행 권한 부여
+chmod +x start.sh
+
+# 시작 스크립트 실행
+./start.sh
+
