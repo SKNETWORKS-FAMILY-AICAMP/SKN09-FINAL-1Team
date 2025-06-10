005d2b7defcf025660dd5d42ecbf33985b0374b3
ohdyo <smart5572@naver.com>
2025-06-10 17:44:29 +0900

    refac: change install_docker.sh in model_hosting

--- model_hosting/install_docker.sh
+++ model_hosting/install_docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt update
+apt update
 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-sudo apt update
+apt update
 
-sudo apt install -y docker-ce
+apt install -y docker-ce
 
 sudo docker --version
