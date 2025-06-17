6a9079a15f67b375d86e2659a16766924292903c
Monkakaka <chojd25@gmail.com>
2025-06-17 11:01:17 +0900

    Test: qdrant test

--- model_hosting/fast_api.py
+++ model_hosting/fast_api.py
@@ -8,11 +8,11 @@ import os
 import torch
 import whisperx
 import ollama
-from ..qdrant_db.qdrant_loader import load_qdrant_db, store_temp_embedding, delete_collection
-from extraction.file_base_extraction import get_extractor_by_extension
-from extraction.prompt_extraction import PromptExtraction
-from ollama_load.ollama_hosting import OllamaHosting
-from module.module import feedback_model, State, TextRequest, QuestionInput, EmbeddingManager, MemoryTools, MySQLCheckpoint, MemoryAgent, search_web_duckduckgo, summarize_body, clean_korean_only, classify_question_mode, get_from_state, split_audio, transcribe_chunk, process_audio_and_extract_qna
+from qdrant_db.qdrant_loader import load_qdrant_db, store_temp_embedding, delete_collection
+from model_hosting.extraction.file_base_extraction import get_extractor_by_extension
+from model_hosting.extraction.prompt_extraction import PromptExtraction
+from model_hosting.ollama_load.ollama_hosting import OllamaHosting
+from model_hosting.module.module import feedback_model, State, TextRequest, QuestionInput, EmbeddingManager, MemoryTools, MySQLCheckpoint, MemoryAgent, search_web_duckduckgo, summarize_body, clean_korean_only, classify_question_mode, get_from_state, split_audio, transcribe_chunk, process_audio_and_extract_qna
 
 
 
