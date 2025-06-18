2fd261fedffd1e248fe2fe85b02e4215843c38c4
Monkakaka <chojd25@gmail.com>
2025-06-18 15:06:56 +0900

    test9

--- model_hosting/main.py
+++ model_hosting/main.py
@@ -2,33 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from model_hosting.fast_api import router, generate_unanswered
-from model_hosting.module.module import init_qdrant_from_call_db
 from dotenv import load_dotenv
 import os
 from contextlib import asynccontextmanager
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
-scheduler = BackgroundScheduler()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-
-    # 매일 오후 6시 실행 스케줄 등록
-    scheduler.add_job(
-        func=init_qdrant_from_call_db,
-        trigger="cron",
-        hour=18,
-        minute=0,
-        id="daily_qdrant_refresh"
-    )
-
-    scheduler.start()
-    yield
-    scheduler.shutdown()
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 load_dotenv() 
 secret = os.getenv("SESSION_SECRET", "default_key")
 
